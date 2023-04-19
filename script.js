90f322ccb63922f17fdee7a02869fff3c3e1a449
LeonardoXavier1 <leonardomx500@gmail.com>
2023-04-19 00:45:26 -0300

    Personagens Harry Potter Site

---
+++ script.js
@@ -0,0 +1,22 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.conteudo-principal-escrito-titulo', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.conteudo-principal-escrito-titulo3', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.conteudo-principal-escrito-subtitulo', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.conteudo-principal-imagem', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.conteudo-principal-imagem3', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.cabecalho-menu-item', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.cabecalho-imagem', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.rodape-imagem', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.linha-do-meio', { duration: 2000, rotate: { x:0, y:30, z:0 }});
+
+sr.reveal('.rodape', { duration: 2000, rotate: { x:0, y:30, z:0 }});
